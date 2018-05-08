fa2c93fed87fbc48895b88b4520eb69525e047f2
rachan leevichit <save.save365@icloud.com>
2018-05-08 15:13:44 +0700

    update data

--- src/oopmit/hello.java
+++ src/oopmit/hello.java
@@ -2,6 +2,10 @@ package oopmit;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println ("hello,my name is Rachan ");
+        System.out.println ("Hello,my name is Rachan ");
+        System.out.println("Major: information system");
+        System.out.println("Faculty : Manament Technology");
+        System.out.println("University : RMUTSV");
+
     }
 }
