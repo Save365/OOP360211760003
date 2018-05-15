96137e50cdefacd79ae1dbd3b0806c007569af63
LabCom_4 <save.save365@icloud.com>
2018-05-15 14:19:43 +0700

    input data form user (Buffer)

--- src/lab7/studentApp.java
+++ src/lab7/studentApp.java
@@ -1,9 +1,12 @@
 package lab7;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class studentApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // s1
         student s1 = new student();
         s1.setName("Mit");
@@ -17,11 +20,29 @@ public class studentApp {
 
         // input data form user s3
         student s3 = new student();
-        s3 = inputdata(s3);
+        //s3 = inputdata(s3);
+        //Buffer แปลงค่า s3
+        s3 = inputdatabuffer(s3);
         System.out.println(s3.toString());
 
 
     }//main
+
+    private static student inputdatabuffer(student s3) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please enter your information :");
+        System.out.println("Name");
+        s3.setName(reader.readLine());
+        System.out.println("age");
+        // แปลง ข้อมูล int เป็น สตริง
+        s3.setAge(Integer.parseInt(reader.readLine()));
+        System.out.println("gender");
+        s3.setGender(reader.readLine());
+        System.out.println("hight");
+        // แปลง ข้อมูล double เป็น สตริง
+        s3.setHeight(Double.parseDouble(reader.readLine()));
+        return s3;
+    }
 
     // ค่าของ s3
     private static student inputdata(student s3) {
