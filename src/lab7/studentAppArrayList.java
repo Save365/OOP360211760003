d114c9fa13c54224479138b53144484256b36792
LabCom_4 <save.save365@icloud.com>
2018-05-15 15:29:26 +0700

    put Object to array list class update

--- src/lab7/studentAppArrayList.java
+++ src/lab7/studentAppArrayList.java
@@ -12,20 +12,33 @@ public class studentAppArrayList {
         int val = Integer.parseInt(reader.readLine());
         ArrayList<student>myList =new ArrayList<student>();
      myList=inputData(myList,val);
+        showData(myList);
     }//main
+
+    private static void showData(ArrayList<student> myList) {
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println("student info " + (i+1)+":");
+            System.out.println("name :"+ myList.get(i).getName());
+            System.out.println("age :" + myList.get(i).getAge());
+            System.out.println("gender:" + myList.get(i).getGender());
+            System.out.println("hight :" + myList.get(i).getHeight());
+        }
+
+
+    }
 
     private static ArrayList inputData(ArrayList<student> myList, int val) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 1; i <= val; i++) {
             student s = new student();
             System.out.println("Enter student info "+i+":");
-            System.out.println("Name");
+            System.out.print("Name");
             s.setName(reader.readLine());
-            System.out.println("age");
+            System.out.print("age");
             s.setAge(Integer.parseInt(reader.readLine()));
-            System.out.println("gender");
+            System.out.print("gender");
             s.setGender(reader.readLine());
-            System.out.println("hight");
+            System.out.print("hight");
             s.setHeight(Double.parseDouble(reader.readLine()));
             myList.add(s);
         }
